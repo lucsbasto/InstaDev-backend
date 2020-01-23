a18f8ba7a90c6680b730b15493616caf9b07a061
Lucas Bastos <lucsbasto@gmail.com>
2020-01-23 10:46:30 -0300

    backend do tindev

---
+++ src/controllers/DislikeController.js
@@ -0,0 +1,12 @@
+const Post = require('../models/Post');
+module.exports = {
+  async store(req, res) {
+    const post = await Post.findOne({
+      _id: req.params.id
+    });
+    post.likes -= 1;
+    await post.save();
+    req.io.emmit('dislike', post);
+    return res.send(post);
+  }
+}
